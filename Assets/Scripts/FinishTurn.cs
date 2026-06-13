8742b1f3b3f343d78a7e6465262565798df406f5
SamuelBamuel <samdinardi@gmail.com>
2026-06-12 23:18:30 -0400

    robust sim phase

--- Assets/Scripts/FinishTurn.cs
+++ Assets/Scripts/FinishTurn.cs
@@ -5,6 +5,7 @@ public class FinishTurn : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public PlayDesignManager PlayDesignManager;
     public GameManager GameManager;
+    public PosBank posBank;
     public void EndTurn() //called by button, increments turn 
     {
         GameManager.currentTurn++;
@@ -12,6 +13,14 @@ public class FinishTurn : MonoBehaviour
         // APlayDesignManager = GetComponent<APlayDesignManager>();
 
         PlayDesignManager.resetLayers(GameManager.currentTeam); //resets to unmoved so players can be moved again
+        if(GameManager.currentTeam == TeamUp.TeamA)
+        {
+            posBank.savePositions(PlayDesignManager.TeamAPlayers);
+        }
+        else
+        {
+            posBank.savePositions(PlayDesignManager.TeamBPlayers);
+        }
     }
 
 }
--- Assets/Scripts/PosBank.cs
+++ Assets/Scripts/PosBank.cs
@@ -4,36 +4,27 @@ public class PosBank: MonoBehaviour
 {
     // public List<(GameObject, Vector3)> teamAStartingPositions = new List<(GameObject, Vector3)>();
     // public List<(GameObject, Vector3)> teamBStartingPositions = new List<(GameObject, Vector3)>();
-    public List<List<(GameObject, Vector3)>> turnPositionsA = new List<List<(GameObject, Vector3)>>();
-    public List<List<(GameObject, Vector3)>> turnPositionsB = new List<List<(GameObject, Vector3)>>();
+    public static List<List<(GameObject, Vector3)>> turnPositionsA = new List<List<(GameObject, Vector3)>>(); //lists of lists to store positions every turn, index 0 will be starting positions
+    public static List<List<(GameObject, Vector3)>> turnPositionsB = new List<List<(GameObject, Vector3)>>(); 
     public void savePositions(GameObject team)
     {
 
-            List<(GameObject, Vector3)> positions = new List<(GameObject, Vector3)>();
-            for (int i = 0; i < team.transform.childCount; i++)
-            {
-                positions.Add((team.transform.GetChild(i).gameObject, team.transform.GetChild(i).position));
-            }
-            if(team.CompareTag("TeamA"))
-            {
-                turnPositionsA.Add(positions);
-                Debug.Log("Saved positions for Team A:");
-                foreach (var (player, position) in positions)
-                {
-                    Debug.Log($"{player.name} at {position}");
-                }
-            }
-            else if(team.CompareTag("TeamB"))
-            {
-                turnPositionsB.Add(positions);
-            }
-            
+        List<(GameObject, Vector3)> positions = new List<(GameObject, Vector3)>(); //this is the sub-list in the lists defined on line 7 and 8
+        for (int i = 0; i < team.transform.childCount; i++)
+        {
+            positions.Add((team.transform.GetChild(i).gameObject, team.transform.GetChild(i).position)); //adds each players position to the list as a tuple of (player, position)
+        }
 
 
-        // Debug.Log("Saved starting positions for Team A:");
-        // foreach (var (player, position) in teamAStartingPositions)
-        // {
-        //     Debug.Log($"{player.name} at {position}");
-        // }
-    }
+        if(team.CompareTag("TeamA")) //makes sure saved to correct list
+        {
+            turnPositionsA.Add(positions);
+
+        }
+        else if(team.CompareTag("TeamB"))
+        {
+            turnPositionsB.Add(positions);
+        }
+    }        
+
 }
